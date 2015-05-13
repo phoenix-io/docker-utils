4b6a8f42c1891f7b1895dffa875d529768f5a8e6
Kunal Kushwaha <kunal.kushwaha@gmail.com>
2015-05-13 20:07:17 +0900

    inital commit
    Signed-off-by: Kunal Kushwaha <kunal.kushwaha@gmail.com>

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"github.com/UmbrellaOps/docker-utils/utils"
+	"github.com/codegangsta/cli"
+	"log"
+	"os"
+)
+
+func main() {
+
+	app := cli.NewApp()
+	app.Name = "docker-utils"
+	app.Usage = "Toolchain for docker"
+	app.Commands = []cli.Command{
+		{
+			Name:  "clean",
+			Usage: "deletes the docker images",
+			Subcommands: []cli.Command{
+				{
+					Name:    "images",
+					Aliases: []string{"I"},
+					Usage:   "remove untag Docker Images",
+					Action: func(c *cli.Context) {
+						removeImages(c)
+					},
+				},
+				{
+					Name:    "container",
+					Aliases: []string{"C"},
+					Usage:   "remove an stopped containers",
+					Action: func(c *cli.Context) {
+						removeContainers(c)
+					},
+				},
+			},
+		},
+	}
+
+	app.Run(os.Args)
+}
+
+// remove images based on given arguments
+func removeImages(c *cli.Context) bool {
+	log.Println("NOT IMPLEMENTED")
+	return false
+}
+
+// delete containers which are not running
+func removeContainers(c *cli.Context) bool {
+	utils.RemoveDockerContainers(true)
+	return false
+
+}
---
+++ utils/docker-util.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"github.com/samalba/dockerclient"
+	"log"
+	"strings"
+)
+
+func RemoveDockerContainers(test bool) {
+	// Init the client
+	docker, err := dockerclient.NewDockerClient("unix:///var/run/docker.sock", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get only running containers
+	containers, err := docker.ListContainers(true, false, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Following containers will be deleted")
+	for _, c := range containers {
+		if strings.Contains(c.Status,"Exit") {
+			log.Println(c.Names)
+		}
+	}
+	if test {
+		log.Println("Test - No action taken")
+	}
+}
