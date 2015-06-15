f5ff55260fff5e1531f6b7eb7537b1659f6cc3e6
Kunal Kushwaha <kunal.kushwaha@gmail.com>
2015-06-15 18:25:10 +0900

    WIP for container deletion
    Signed-off-by: Kunal Kushwaha <kunal.kushwaha@gmail.com>

--- utils/docker-util.go
+++ utils/docker-util.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"github.com/samalba/dockerclient"
 	"log"
+	"fmt"
 	"strings"
 )
 
@@ -22,26 +23,75 @@ func InitUtilContext() (*UtilContext, error) {
 
 }
 
-func (ctx *UtilContext) RemoveDockerContainers(test bool) error {
+func (ctx *UtilContext) DeleteSingleContainer(container string, id bool) error {
+
+	// Get the container
+	var filter string
+	if id {
+		fmt.Sprintf(filter, "{'id':['%s']}", container)
+	} else {
+		fmt.Sprintf(filter, "{'name':[%s']}", container)
+	}
+	containers, err := ctx.client.ListContainers(true, false, filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if dry_run {
+		log.Println("dry-run - Skipping deletion of containers")
+	}
+	if len(containers) > 1 {
+		log.Println("More then one option found with same id or name.")
+		return error("More then one option found with same id or name")
+	}
+
+	err = DeleteContainer(containers, "")
+
+	return err
+
+
+
+}
+
+func (ctx *UtilContext) DeleteExitedContainers(dry_run bool) error {
 
 	// Get all containers
 	containers, err := ctx.client.ListContainers(true, false, "")
 	if err != nil {
 		log.Fatal(err)
 	}
+	if dry_run {
+		log.Println("dry-run - Skipping deletion of containers")
+	}
+
 	log.Println("Following containers will be deleted")
-	for _, c := range containers {
-		if strings.Contains(c.Status, "Exit") {
-			log.Println(c.Names)
-		}
-	}
-	if test {
-		log.Println("Test - No action taken")
-	}
+
+	err = DeleteContainer(containers, "Exited")
+
 	return err
 }
 
-func (ctx *UtilContext) RemoveDockerImages(test bool) error {
+func (ctx *UtilContext) DeleteContainer(containerList *dockerclient.Container,status string) (err error) {
+
+	for _, c := range containerList {
+		if strings.Contains(c.Status, status) {
+			log.Println(c.Names)
+			if !dry_run {
+				err = ctx.client.RemoveContainer(c.ID, true, true)
+				if err != nil {
+					log.Println(err)
+				}
+			}
+		}
+	}
+	return err
+
+}
+
+func (ctx *UtilContext) RemoveDockerImage(image string, id bool) error {
+
+}
+
+func (ctx *UtilContext) RemoveUntaggedDockerImages(dry_run bool) error {
 
 }
 
